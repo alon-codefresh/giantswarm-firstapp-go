3c67032ccf21cb9195344e149554ff92696eb44e
Matthias Luebken <matthias.luebken@gmail.com>
2015-08-19 19:19:05 +0200

    cleanup and comments

--- startredis.sh
+++ startredis.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-mkdir /tmp2
-chown redis:redis /tmp2
-#TODO exec
-redis-server /usr/local/etc/redis/redis.conf
+
+# make the /tmp folder writable for redis server to create the socket.
+# has to be done here. after the volume is mountet as root
+chown redis:redis /tmp
+
+# start with exec for correct signal handling
+exec redis-server /usr/local/etc/redis/redis.conf
